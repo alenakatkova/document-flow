a3d5ef215d29c66e9c62150e6d25d8176a437c61
Alena Katkova <katkova.web@gmail.com>
2022-08-30 19:52:58 +0300

    start styling layout

--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -1,27 +1,35 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 import Head from "next/head";
 import Link from "next/link";
-import {NextPage} from "next";
-import {UserFromDB} from "../interfaces/user";
+import SideMenu from "./SideMenu";
+
+import * as React from "react";
+import {styled} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
 
 // example from Next.js https://github.com/vercel/next-learn/blob/master/basics/typescript-final/components/layout.tsx
 
 interface LayoutProps {
-  children: ReactNode;
-  title: string;
+  children : ReactNode;
+  title : string;
 }
 
-const Layout = ({ children, title }: LayoutProps) => {
+const Layout = ({children, title} : LayoutProps) => {
   return (
       <div>
         <Head>
           <title>{title}</title>
         </Head>
-        <header>
-          HEADER
-        </header>
-        <main>{children}</main>
-        <footer>FOOTER</footer>
+        <Grid container spacing={0} wrap="nowrap" minHeight="100vh">
+          <Grid xs={3} lg={2} minWidth={230}>
+            <SideMenu/>
+          </Grid>
+          <Grid xs={9} lg={10}>
+            {children}
+          </Grid>
+        </Grid>
       </div>
   )
 }
