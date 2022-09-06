a08087423d8e5f2b161be0768f30147abec2210b
Alena Katkova <katkova.web@gmail.com>
2022-09-06 05:27:36 +0300

    delete counterparty instance

--- server/routes/counterpartyType.js
+++
@@ -1,6 +0,0 @@
-const counterpartyTypeController = require("../controllers/counterpartyType.controller");
-const router = require("express").Router();
-
-router.get("/", counterpartyTypeController.findAll);
-
-module.exports = router;
